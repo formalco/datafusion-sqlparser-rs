4e643b49afc0b3a83c91d9ef8c1da841a69fc403
Guillaume Gelin <contact@ramnes.eu>
2026-05-12 16:29:00 +0200

    Snowflake: Add support for PUT
    This PR adds a `Statement::Put` variant and parses the documented grammar:
    
    ```sql
    PUT 'file://<path>' <internalStage> [ <option> = <value> ... ]
    ```
    
    https://docs.snowflake.com/en/sql-reference/sql/put

--- src/ast/spans.rs
+++ src/ast/spans.rs
@@ -503,7 +503,7 @@ impl Spanned for Statement {
             Statement::Print { .. } => Span::empty(),
             Statement::WaitFor(_) => Span::empty(),
             Statement::Return { .. } => Span::empty(),
-            Statement::List(..) | Statement::Remove(..) => Span::empty(),
+            Statement::List(..) | Statement::Put { .. } | Statement::Remove(..) => Span::empty(),
             Statement::ExportData(ExportData {
                 options,
                 query,
